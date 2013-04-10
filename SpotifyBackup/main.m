c41148a96fce3234c2c0a6995d5628d56c914dd7
Max Woolf <max.woolf@boxuk.com>
2013-04-10 10:54:53 +0100

    Initial Commit

---
+++ SpotifyBackup/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SpotifyBackup
+//
+//  Created by Max Woolf on 10/04/2013.
+//  Copyright (c) 2013 MaxWoolf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
